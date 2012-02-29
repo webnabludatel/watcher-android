be1d7bae5c874e0a8b865a76d0807b2df4d1f3ae
Alexey Efimov <aefimov-box@ya.ru>
2012-03-01 00:53:34 +0400

    Add RU regions

--- src/org/dvaletin/apps/nabludatel/utils/DistrictRegionAdapter.java
+++ src/org/dvaletin/apps/nabludatel/utils/DistrictRegionAdapter.java
@@ -1,43 +1,36 @@
 package org.dvaletin.apps.nabludatel.utils;
 
-import java.util.ArrayList;
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 
 public class DistrictRegionAdapter extends BaseAdapter {
-	private ArrayList<DistrictRegion> regions;
+	private DistrictRegion[] regions;
 	private LayoutInflater mInflater;
 	
-	public DistrictRegionAdapter(Context context, ArrayList<DistrictRegion> items){
-		regions = items;
-		mInflater = LayoutInflater.from(context);
+	public DistrictRegionAdapter(Context context, DistrictRegion... regions){
+		this.regions = regions;
+		this.mInflater = LayoutInflater.from(context);
 	}
 	@Override
 	public int getCount() {
-		// TODO Auto-generated method stub
-		return regions.size();
+		return regions.length;
 	}
 
 	@Override
-	public Object getItem(int position) {
-		// TODO Auto-generated method stub
-		return null;
+	public String getItem(int position) {
+		return regions[position].getTitle();
 	}
 
 	@Override
 	public long getItemId(int position) {
-		// TODO Auto-generated method stub
-		return 0;
+		return regions[position].getId();
 	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 }
